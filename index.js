af2e44170bfd271a87b8bbac20750dfb8625e905
Swagata Debnath <swagmu0309@gmail.com>
2023-10-11 21:13:59 +0530

    changes

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-const express = require('express');
+const http = require('http');
+const fs = require('fs');
 const path = require('path');
 
-const app = express();
-const port = 3000;
+const server = http.createServer((req, res) => {
+  let filePath = '.' + req.url;
 
-app.use(express.static(path.join(__dirname, 'public')));
+  if (filePath === './') {
+    filePath = './index.html';
+  }
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  const extname = path.extname(filePath);
+  let contentType = 'text/html';
+
+  if (extname === '.css') {
+    contentType = 'text/css';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('500 Internal Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(data, 'utf8');
+    }
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+const port = 3000;
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
